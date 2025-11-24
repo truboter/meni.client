b0de493f7133f868074534be8b0467bee4088e55
Andrei <truboter@gmail.com>
2025-11-24 23:27:36 +0400

    Align menu and language buttons to the left

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -159,7 +159,7 @@ export function SettingsMenu({
 
   return (
     <>
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-start gap-2">
         <div className="flex items-center gap-2">
           <Button
             variant="secondary"
